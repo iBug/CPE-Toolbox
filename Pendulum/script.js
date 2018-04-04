b212d6fdd706cf3d04f4c166a8ec6a079ae1d15b
iBug <7273074+iBug@users.noreply.github.com>
2018-04-04 16:51:11 +0800

    Add a testing output

--- Pendulum/script.js
+++ Pendulum/script.js
@@ -1,5 +1,4 @@
 var btnAddData = document.getElementById("addData");
-
 btnAddData.addEventListener("click", e => e.preventDefault());
 btnAddData.onclick = function() {
   let tr = makeElement("tr"), td;
@@ -17,4 +16,48 @@ btnAddData.onclick = function() {
   tr.appendChild(td);
   var addDataBefore = document.getElementById("addDataBefore");
   addDataBefore.parentElement.insertBefore(tr, addDataBefore);
+};
+
+var btnShowResult = document.getElementById("showResult");
+showResult.addEventListener("click", e => e.preventDefault());
+showResult.onclick = function() {
+  var sLens = document.querySelectorAll('input[name="sLen"]'),
+    bDiams = document.querySelectorAll('input[name="bDiam"]'),
+    wTimes = document.querySelectorAll('input[name="wTime"]');
+  var vl = [], vd = [], vT = [], vN = Number(document.getElementById("N").value);
+  for (let i = 0; i < sLens.length; i++) {
+    vl.push(Number(sLens[i].value));
+  }
+  for (let i = 0; i < bDiams.length; i++) {
+    vd.push(Number(bDiams[i].value));
+  }
+  for (let i = 0; i < wTimes.length; i++) {
+    vT.push(Number(wTimes[i].value));
+  }
+  output.clear();
+  output.print("Average string length: " + average(vl) + "\n");
+  output.print("Average ball diagram: " + average(vd) + "\n");
+  output.print("Average wave time: " + average(vT)/vN + "\n");
+};
+
+function average(l) {
+  return sum(l) / l.length;
+}
+
+function sum(l, s = 0) {
+  for (let i = 0; i < l.length; i++) {
+    s += l[i];
+  }
+  return s;
+}
+
+var output = {
+  field: document.getElementById("output"),
+  clear: function() {
+    this.field.innerText = "";
+  },
+  print: function(str) {
+    this.field.innerText += str;
+    return str;
+  }
 };
