31523020a4c828956b30ed9151d577d3d1f5a43f
iBug <stevepad@me.com>
2018-04-13 16:07:32 +0800

    Update Shear Modulus

--- ShearModulus/script.js
+++ ShearModulus/script.js
@@ -15,15 +15,25 @@ btnAddData.onclick = function() {
 var btnShowResult = document.getElementById("showResult");
 showResult.addEventListener("click", e => e.preventDefault());
 showResult.onclick = function() {
-  let iL = getNumbers("iL"),
-    iD = getNumbers("iD"),
-    im = getNumbers("im"),
-    iri = getNumbers("iri"),
-    iro = getNumbers("iro"),
-    iT0 = getNumbers("iT0"),
-    iT1 = getNumbers("iT1"),
+  let iL = getNumbers("iL"), // cm
+    iD = getNumbers("iD"), // mm
+    im = getNumbers("im"), // g
+    iri = getNumbers("iri"), // cm
+    iro = getNumbers("iro"), // cm
+    iT0 = getNumbers("iT0"), // s
+    iT1 = getNumbers("iT1"), // s
     iN0 = getNumber("iN0"),
     iN1 = getNumber("iN1");
+
+  let L = iMath.average(iL),
+    m = iMath.average(im),
+    T0 = iMath.average(iT0),
+    T1 = iMath.average(iT1),
+    ri = iMath.average(iri),
+    ro = iMath.average(iro),
+    R = iMath.average(iD) / 2;
+  let G = 4 * Math.PI * L * m * (ri * ri + ro * ro) / ((T1 * T1 - T0 * T0) * Math.pow(R, 4))
+
   output.clear();
   output.print("Average string length: " + iMath.average(iL));
   output.print("Average string radius: " + iMath.average(iD)/2);
@@ -32,5 +42,6 @@ showResult.onclick = function() {
   output.print("Average outer radius: " + iMath.average(iro));
   output.print("Average cycle without ring: " + iMath.average(iT0)/iN0);
   output.print("Average cycle with ring: " + iMath.average(iT1)/iN1);
+  output.print("Result: G = " + G);
 };
 
