192df26d93be1589697a9d2bcc5087e64ee274f9
TaoKY <taoky1999@gmail.com>
2018-08-04 22:10:32 +0800

    localize & redesign UI of Pendulum

--- Pendulum/script.js
+++ Pendulum/script.js
@@ -7,11 +7,11 @@ showResult.onclick = function() {
   var vl = toNumArray(sLens), vd = toNumArray(bDiams), vT = toNumArray(wTimes),
     vN = Number(document.getElementById("N").value);
   output.clear();
-  output.print("Average string length: " + iMath.average(vl));
-  output.print("Average ball diagram: " + iMath.average(vd));
-  output.print("Average wave time: " + iMath.average(vT)/vN);
-  output.print("Standard deviation of string length: " + iMath.stddev(vl));
-  output.print("Standard deviation of ball diagram: " + iMath.stddev(vd));
-  output.print("Standard deviation of wave time: " + iMath.stddev(vT)/vN);
+  output.print("平均摆线长 (cm): " + iMath.average(vl));
+  output.print("平均摆球直径 (mm): " + iMath.average(vd));
+  output.print("平均时间 (s): " + iMath.average(vT)/vN);
+  output.print("摆线长标准差 (cm): " + iMath.stddev(vl));
+  output.print("摆球直径标准差 (mm): " + iMath.stddev(vd));
+  output.print("总时间标准差 (s): " + iMath.stddev(vT)/vN);
 };
 
